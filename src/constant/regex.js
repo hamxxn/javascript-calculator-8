fa9de8478a7de9ddd6ffeb79e5adc46af2528b40
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-10-20 16:03:59 +0900

    fix(constant): custom 정규표현식 수정

--- src/constant/regex.js
+++ src/constant/regex.js
@@ -5,7 +5,5 @@ export const CUSTOM_DELIMITER = {
 
 export const REGEX_PATTERNS = {
   BASIC_DELIMITER_FORMAT: /^[1-9]\d*(?:[,:]+[1-9]\d*)*$/,
-  CUSTOM_DELIMITER_FORMAT: new RegExp(
-    `^${CUSTOM_DELIMITER.START}([^\d-]+)${CUSTOM_DELIMITER.END}(.*)?$`
-  ),
+  CUSTOM_DELIMITER_FORMAT: /^\/\/([^\d-]+)\\n(.*)?$/,
 };
