affea9fcd8897c0a8cb4bf7ea105093c29bb20e7
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-10-20 14:39:09 +0900

    feat(App): 클래스 정의 및 의존성 주입
    - 전체 애플리케이션 실행 흐름을 담당할 App 클래스 기본 구조 정의
    -IOHandler와 StringParser 인스턴스를 생성자에서 주입

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { Console } from "@woowacourse/mission-utils";
+import IOHandler from "./IOHandler.js";
+import StringParser from "./StringParser.js";
+
 class App {
-  async run() {}
+  constructor() {
+    this.ioHandler = new IOHandler();
+    this.stringParser = new StringParser();
+  }
+
+  async run() {
+    try {
+      const input = await this.ioHandler.input();
+      const result = this.stringParser.calculate(input);
+
+      await this.ioHandler.print(result);
+    } catch (error) {
+      Console.print(error.message);
+      throw error;
+    }
+  }
 }
 
 export default App;
