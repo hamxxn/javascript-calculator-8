c50167ff650379d672a0de3e60fb4ce8e387b9fa
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-10-20 14:30:36 +0900

    feat(constant): 에러 메세지 상수 정의
    -입력 검증 실패 시 출력할 에러 메시지 상수로 분리

---
+++ src/constant/error.js
@@ -0,0 +1,6 @@
+export const ERROR_MESSAGES = {
+  INVALID_INPUT:
+    '[ERROR] 입력 형식을 다시 확인해 주세요. (ex. "1,2,3", "1:2:3, 1,2:3", "//;\\n1;2;3")',
+  INVALID_CUSTOM_DELIMITER:
+    '[ERROR] 커스텀 구분자는 숫자나 하이픈(-)이 아닌 문자를 사용할 수 있어요. (ex. "//;\\n1;2;3", "//@@\\n1@@2@@3")',
+};
